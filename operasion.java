51e3ac5c3a0681228b812a11860f771e4dd16e29
Geydarova Zulfiya <152681616+Zulya1012@users.noreply.github.com>
2024-01-09 16:15:23 +0400

    Update operasion.java

--- operasion.java
+++ operasion.java
@@ -1,12 +1,12 @@
-public class operasion{
-	public static void main(String[] arg){
-		int a = 5;
-		a += 10;        
-		a-= 3;         
-		a *= 2;         
-		a /= 6;         
-		a <<= 4;     
-		a >>= 2;      
-			System.out.println(a); 
-	}
+public class operasion {
+  public static void main(String[] arg) {
+    int a = 5;
+    a += 10;
+    a -= 3;
+    a *= 2;
+    a /= 6;
+    a <<= 4;
+    a >>= 2;
+    System.out.println(a);
+  }
 }
